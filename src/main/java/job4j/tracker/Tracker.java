e1230b46d0d267725da0290d118664778fcd1a5f
Gerik228 <dimaakulichd6618@gmail.com>
2025-10-06 11:45:55 +1000

    Замена заявки с сохранением id

--- src/main/java/job4j/tracker/Tracker.java
+++ src/main/java/job4j/tracker/Tracker.java
@@ -1,5 +1,4 @@
 package job4j.tracker;
-
 import java.util.Arrays;
 
 public class Tracker {
@@ -12,35 +11,50 @@ public class Tracker {
         items[size++] = item;
         return item;
     }
-
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int result = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                result = index;
                 break;
             }
         }
-        return rsl;
+        return result;
+    }
+    public boolean replace (int id, Item item){
+        int  index = indexOf(id);
+        if (index != -1) {item.setId(id);
+            items[index] = item;
+        }else {
+            return false;
+        }
+        return true;
     }
 
-    public String[] findByName(String key){
+    public Item findById(int id) {
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
+    }
 
+    public Item[] findByName(String key){
         int counter = 0;
-        String[] itemsSh = new String[100];
-
-        for (int i = 0; i < items.length; i++) {
+        Item[] itemsSh = new Item[100];
+        for (int i = 0; i < size; i++) {
             if(items[i].getName().equals(key) ){
-                itemsSh[counter] = items[i].getName();
+                itemsSh[counter] = items[i];
                 counter++;
             }
-
         }
-
-
         return Arrays.copyOf(itemsSh, counter);
     }
+
+    public Item[] findAll(){
+        return Arrays.copyOf(items, size);
+    }
+
+
 
 
 }
