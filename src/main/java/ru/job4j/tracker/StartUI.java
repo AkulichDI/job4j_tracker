6e7a69c38360e890f250aaf433dbf2a043c1e01c
Gerik228 <dimaakulichd6618@gmail.com>
2025-10-23 12:55:48 +1000

    1. Singleton В работе

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -43,6 +43,10 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+
+        Log4File log = Log4File.getInstance();
+        log.add("add new Item");
+        log.save();
         Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
