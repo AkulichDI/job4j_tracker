2d894acac8ab9b1ccafb583a19eed9e49e3cf5cc
Gerik228 <dimaakulichd6618@gmail.com>
2025-10-28 12:37:46 +1000

    9. Изменить программу Tracker из 2-го модуля (Делаю)

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,21 +1,24 @@
 package ru.job4j.tracker;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    //private final Item[] items = new Item[100];
     private int ids = 1;
-    private int size = 0;
+    ArrayList<Item> items = new ArrayList<>();
+
+
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
     private int indexOf(int id) {
         int result = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
                 result = index;
                 break;
             }
@@ -28,7 +31,7 @@ public class Tracker {
             return false;
         }
         item.setId(id);
-        items[index] = item;
+        items.add(index,item);
         return true;
     }
     public boolean delete (int id){
@@ -36,9 +39,10 @@ public class Tracker {
         if (index == -1) {
             return false;
         }
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
+        items.remove(index);
+//        System.arraycopy(items, index + 1, items, index, items.size() - index - 1);
+//        items[size - 1] = null;
+//        size--;
         return true;
     }
 
@@ -46,23 +50,24 @@ public class Tracker {
         /* Находим индекс */
         int index = indexOf(id);
         /* Если индекс найден возвращаем item, иначе null */
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     public Item[] findByName(String key){
         int counter = 0;
-        Item[] itemsSh = new Item[100];
-        for (int i = 0; i < size; i++) {
-            if(items[i].getName().equals(key) ){
-                itemsSh[counter] = items[i];
+        ArrayList<Item> itemsSh = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            if(items.get(i).getName().equals(key) ){
+                itemsSh.set(counter, items.get(i));
                 counter++;
             }
         }
+
         return Arrays.copyOf(itemsSh, counter);
     }
 
     public Item[] findAll(){
-        return Arrays.copyOf(items, size);
+        return Arrays.copyOf(items, items.size());
     }
 
 
